1a4c2fab156f197b2012cb86f5ebafc1d597ac99
Lutztest <lutzmahler@gmx.de>
2016-10-27 17:48:36 +0200

    a

--- app/src/main/java/com/example/jules/kugelspiel/MainActivity.java
+++ app/src/main/java/com/example/jules/kugelspiel/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //this line is new aka second commit/push
         //this line is new aka third only subdir commit/push
+        int i=0;
         //in browser change
     }
 }
